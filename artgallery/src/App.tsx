5ce1c15f3556765f7e22423aa04149a9721f5bf3
diplomat2805 <insightdiplomat@gmail.com>
2025-11-30 01:07:43 +0530

    Added Shipping returns , Privacy policy , Contact , terms and condition Page

--- artgallery/src/App.tsx
+++ artgallery/src/App.tsx
@@ -17,8 +17,12 @@ import { AppProvider, useApp } from './context/AppContext';
 import { NewsletterPopup } from './components/NewsletterPopup';
 import { Toaster } from './components/ui/sonner';
 
-// ⭐ NEW IMPORT FOR SHIPPING PAGE
+// ⭐ NEW IMPORT FOR SHIPPING PAGE & PRIVACY POLICY & TERMS AND CONDITION
 import ShippingReturnsPage from './components/pages/ShippingReturnsPage';
+import PrivacyPolicyPage from './components/pages/PrivacyPolicyPage';
+import TncPage from './components/pages/TncPage';
+import ContactPage from './components/pages/ContactPage';
+
 
 // 🔒 LOGIN REQUIRED
 function RequireAuth({ children }: { children: JSX.Element }) {
@@ -53,8 +57,13 @@ export default function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
 
-            {/* ⭐ NEW SHIPPING & RETURNS ROUTE */}
+            {/* ⭐ NEW SHIPPING & RETURNS ROUTE & Privacy Policy Route & Terms and Condition Route & Contact page rooute*/}
             <Route path="/shipping-returns" element={<ShippingReturnsPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/terms" element={<TncPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+
+
 
             {/* 🔓 CART IS PUBLIC */}
             <Route path="/cart" element={<CartPage />} />
